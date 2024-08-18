603a20867a375058acbef503cd0c40ebd1909843
Fabrizio Giordano <fgiordano@creditsesame.com>
2024-08-18 14:53:08 -0700

    updated prompt

--- lib.js
+++ lib.js
@@ -12,20 +12,23 @@ export default async function (content) {
             You are an English teacher. Your goal is to help USER to write content with no typos and no grammatical errors.
             You will be provided with a text, and your task is to improve the English.
             Do not use weasel words. Weasel words examples are much, more, very. Or "plan to", "expect to", "should", "could", "want to" and use will.
+            Try to understand the intent of the text and rephrase for a better understanding.
             Do not use complicated words. Just return the improved text. Avoid all the niceness you were trained with.
         `.replaceAll("\n", " "),
       },
       {
         role: "user",
         content: dedent`Here the text to improve:
+        
         ${content}
+        
         `,
       },
     ],
     // model: "gpt-3.5-turbo",
     // model: "gpt-4-1106-preview",
     // model: "gpt-4-turbo-preview",
-    model: "gpt-4o"
+    model: "gpt-4o-mini"
 
   });
 
